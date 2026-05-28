0f458c06c779a0f586c6eda0403c9c3fd48f6fa1
jacobthewanderer <jwallach47@gmail.com>
2026-05-28 10:31:35 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="spotify-discography-research-tools-v4";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener("fetch",e=>{const u=new URL(e.request.url);if(u.hostname==="api.spotify.com"||u.hostname==="accounts.spotify.com"||u.hostname==="musicbrainz.org"){e.respondWith(fetch(e.request));return}if(u.origin===location.origin)e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)))});
